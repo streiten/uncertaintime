a4fd58f859db43a1a41195f9dbfff752b07560d4
Alex Tolar <alextolar@Alexs-iMac.local>
2016-07-12 21:29:07 +0200

    sockets and d3 clock

--- app/index.js
+++ app/index.js
@@ -1,16 +1,35 @@
 var express = require('express');
-var mustache = require('mustache-express');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+// var mustache = require('mustache-express');
 app.use(express.static('public'));
 app.get('/',requestHandler);
 
+io.on('connection', function(socket){
+  console.log('A Client connected!');
+  io.emit('welcome','Hi new client!');
+
+  socket.on('disconnect', function(){
+    console.log('A Client disconnected!');
+  });
+});
+
 function requestHandler(request, response) {
-  response.sendfile('./views/index.html');
+  response.sendFile( __dirname + '/views/index.html');
   console.log('Serving another request (sigh)')
 }
 
-app.listen(8888, function(){
-    console.log('Serving you...')
+http.listen(8888, function(){
+    console.log('Serving you since 1st of Jan 1970...')
 });
+
+var timebroadcast = function() { 
+    setTimeout(function() {
+      io.emit('time', new Date());
+    timebroadcast();
+  }
+,1000)};
+
+timebroadcast();
