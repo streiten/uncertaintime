922b8708412ad67befe4ead3729c5bafe785c36c
Alex <alex@rainbow-unicorn.com>
2016-08-15 09:52:12 +0200

    getting 'this' right

--- app/index.js
+++ app/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var uncertainTime = require('./libs/uncertainTime.js');
-// var mustache = require('mustache-express');
+
+var uct = require('./libs/uncertainTime.js');
+var uncertainTime = new uct('m');
+
 app.use(express.static('public'));
 app.get('/',requestHandler);
 
@@ -21,21 +23,21 @@ function requestHandler(request, response) {
   console.log('Serving another request *tick tack*.')
 }
 
-var uct = new uncertainTime('m');
+http.listen(8888, function(){
+    console.log('Tick tacking since 1st of Jan 1970...')
+});
 
 function timebroadcast() { 
   io.emit('time', { 
     unit : 'm',
-    value : uct.time
+    value : uncertainTime.time
   });
 }
 
 setInterval(timebroadcast,1000);
 
+
+
 function clearConsole() {
   process.stdout.write('\x1B[2J\x1B[0f');
 }
-
-http.listen(8888, function(){
-    console.log('Tick tacking since 1st of Jan 1970...')
-});
--- app/libs/uncertainTime.js
+++ app/libs/uncertainTime.js
@@ -2,32 +2,26 @@ var easing = require('easing-js');
 var util = require('util');
 
 function uncertainTime(u) {
-  self = this;
   this.unit = u;
-  setInterval(this.distortTime,100);
-
+  setInterval(this.distortTime.bind(this),100);
 };
 
 uncertainTime.prototype.distortTime = function () {
-      
-      var date = new Date();
-      switch(self.unit) {
-        case 's':
-          var second = date.getSeconds();
-          var distortedSecond = distortFunction(second);
-          date.setSeconds(distortedSecond);
-        break;         
-        case 'm':
-          var minute = date.getMinutes();
-          var distortedMinute = distortFunction(minute);
-          date.setMinutes(distortedMinute);
-        break; 
-      }
-
-  self.time = date;
+  var date = new Date();
+  switch(this.unit) {
+    case 's':
+      var second = date.getSeconds();
+      var distortedSecond = distortFunction(second);
+      date.setSeconds(distortedSecond);
+    break;         
+    case 'm':
+      var minute = date.getMinutes();
+      var distortedMinute = distortFunction(minute);
+      date.setMinutes(distortedMinute);
+    break; 
+  }
+  this.time = date;
 }
-
-
 
 function distortFunction(val) {
   var result = Math.floor(easing.easeInOutQuad(val,0,60,60));
