09c92f413a6b8e8d570e0152131ecf71b8a6083c
Alex <alex@rainbow-unicorn.com>
2017-01-20 18:46:03 +0100

    updates

--- app/libs/uncertainTime.js
+++ app/libs/uncertainTime.js
@@ -1,95 +1,186 @@
+var winston = require('winston');
 var fs = require('fs');
 var later = require('later');
 var easing = require('easing-js');
 var util = require('util');
 var path = require('path');
 var moment = require("moment");
- 
-function uncertainTime() {
-  this.debug = true;
-  // reading in the schedule from file
-  
-  var schedulePath = path.join(__dirname,'..', 'schedule.json');
-  var schedule = JSON.parse(fs.readFileSync(schedulePath, 'utf8'));
-  this.later = later.schedule(schedule);
 
+function uncertainTime() {
+  
+  // reading in the schedule from file
+  winston.log('info', "Init schedule... ");
+
+  var scheduleFile = path.join(__dirname,'..', 'schedule.json');
+  
+  this.schedule = JSON.parse(fs.readFileSync(scheduleFile, 'utf8'));
+  this.initSchedule();
+
+  winston.log('info', "Current/Next Period Start: " + this.start.format());
+  winston.log('info', "Current/Next Period End: " + this.end.format()); 
+  winston.log('info', "Period will last about " + this.end.from(this.start,true)); 
+
+  this.uncertainTimePeriodUpdated = true;
+
+  this.debug = false;
   if(this.debug) {
-    this.initDebug();
+    this.initDebug(2);
   }
 
   this.uncertain = false;  
   this.timederrivation = 0;
+  
   // calculating the time every 100ms
-  setInterval(this.distortTime.bind(this),100);
+  setInterval(this.updateTime.bind(this),100);
+
 }
 
-uncertainTime.prototype.distortTime = function () {
+uncertainTime.prototype.updateTime = function () {
 
       var realTime = moment();
-      
-      if(this.debug){
 
-          // unix() mutability... issue ?
-        var timeInSeconds = distortFunction(realTime.unix(),this.start.unix(),this.end.unix());
+      this.updateCurrentPeriod();
 
-        //console.log("It's the time again:" + uncertainTime );
-        this.time = moment(timeInSeconds);
-        
-        // truncate decimal with | 0 -> makes it 32 bit integer
-        this.uncertain = true;
-        // moment uncertainTime.duration(realtime)
-        //this.timederrivation = ((uncertainTime.getTime() - nowtime.getTime())/1000 ) | 0 ;
+      if(this.debug ||  this.uncertain){
 
-        console.log('Start',this.start);
-        console.log('End',this.end);
-        console.log('RT:',realTime);
-        console.log('UT:',this.time);
-      
+        this.time = distortFunction(realTime,this.start,this.end);
+        this.timederrivation = Math.round(this.time.diff(realTime) / 1000);
+
+        // console.log('Start',this.start);
+        // console.log('End',this.end);
+        // console.log('RT  :',realTime);
+        // console.log('UT  :',this.time);
+        // console.log('DIFF:',this.timederrivation);
+
       } else {
       
-        this.uncertain = false;
         this.timederrivation = 0; 
         this.time = realTime;
       
       }
 };
 
-function distortFunction(val,start,end) {
+uncertainTime.prototype.updateCurrentPeriod = function (){
+
+  if(moment().isBetween(this.start, this.end)) {
   
-  var diff = end - start;
-  var val = val - start; 
+    if(this.uncertainTimePeriodUpdated) {
+      winston.log('info', "A uncertainty period started.");
+    } else {
 
-  console.log('Val:' + val);
-  console.log('Start:' + start);
-  console.log('End:' + end);
-  console.log('Diff:' + diff);
+    }
+      this.uncertainTimePeriodUpdated = false;
 
-  // why floor
-  var result = Math.floor(easing.easeInOutQuad(val,0,diff,diff));  
-  return result + start;
+    this.uncertain = true;
+  
+  } else {
+    
+    winston.log('info', "The uncertainty period ended.");
+    this.uncertain = false;
+
+    if(!this.uncertainTimePeriodUpdated) {
+
+      for (var i = this.schedule.uncertainTimes.length - 1; i >= 0; i--) {
+        var now = moment();
+        // end in past, remove item from schedules array 
+        var isPast = this.schedule.uncertainTimes[i].end.isBefore(now);
+        if(isPast) {
+          this.schedule.uncertainTimes.splice(i,1);
+        }
+      }
+      
+      // let's assume 
+      // - no overlaps in schedule definition 
+      // - chronolgical order 
+      // so after removing past ones the first should be the current or next one
+      if(this.schedule.uncertainTimes.length > 0) {
+        this.start = this.schedule.uncertainTimes[0].start;
+        this.end = this.schedule.uncertainTimes[0].end;
+      } else {
+        // uh no uncertain period coming up...
+        throw new Error('Err... no valid uncertain period defined...');
+      }
+
+      winston.log('info', "Next Period Start: " + this.start.format());
+      winston.log('info', "Next Period End: " + this.end.format()); 
+      winston.log('info', "Period will last about " + this.end.from(this.start,true)); 
+
+      this.uncertainTimePeriodUpdated = true;
+
+    }
+
+  }
+
 }
 
-uncertainTime.prototype.initDebug = function () {
-    // set initial start end
-    if(moment().minutes() % 2 == 0){
-      this.start = moment().startOf('minute');
-    } else {
-      this.start =  moment().startOf('minute').subtract(1,'m');
-    }
-    this.end = this.start.clone().add(2, 'm'); 
+uncertainTime.prototype.initDebug = function(duration) {
 
-   // console.log('Start',this.start);
-   // console.log('End',this.end);
+    // set initial start end
+    var subtractVal = moment().minutes() % duration;
+
+    this.start =  moment().startOf('minute').subtract(subtractVal,'m');
+    this.end = this.start.clone().add(duration, 'm'); 
+
+    console.log('Start',this.start);
+    console.log('End',this.end);
 
     // set start end every 2 mins 
-    debugSchedule = later.parse.text('every 2 min');
+    debugSchedule = later.parse.recur().every(duration).minute();
     t = later.setInterval(setStartEnd.bind(this), debugSchedule);
     function setStartEnd(){
       this.start = moment().startOf('minute');
-      this.end = moment().startOf('minute').add(2, 'm'); 
-        // console.log('Start',this.start);
-        // console.log('End',this.end);
+      this.end = moment().startOf('minute').add(duration, 'm'); 
+        console.log('Start',this.start);
+        console.log('End',this.end);
     };
+
+};
+
+
+function distortFunction(val,start,end) {
+  
+  var diff = end.unix() - start.unix();
+  var val = val.unix() - start.unix(); 
+
+  // console.log('Val:' + val);
+  // console.log('Start:' + start);
+  // console.log('End:' + end);
+  // console.log('Diff:' + diff);
+
+  var result = easing.easeInOutQuad(val,0,diff,diff);  
+  return moment(result * 1000 + start);
+
 }
+
+uncertainTime.prototype.initSchedule = function (){
+
+  for (var i = this.schedule.uncertainTimes.length - 1; i >= 0; i--) {
+    
+    this.schedule.uncertainTimes[i].start = moment(this.schedule.uncertainTimes[i].start);
+    this.schedule.uncertainTimes[i].end = moment(this.schedule.uncertainTimes[i].end);
+    var now = moment();
+
+    // end in past, remove item from schedules array 
+    var isPast = this.schedule.uncertainTimes[i].end.isBefore(now);
+    if(isPast) {
+      this.schedule.uncertainTimes.splice(i,1);
+    }
+
+  }
+  
+  // let's assume 
+  // - no overlaps in schedule definition 
+  // - chronolgical order 
+  // so after removing past ones the first should be the current or next one
+  if(this.schedule.uncertainTimes.length > 0) {
+    this.start = this.schedule.uncertainTimes[0].start;
+    this.end = this.schedule.uncertainTimes[0].end;
+  } else {
+    // uh no uncertain period coming up...
+    throw new Error('Err... no valid uncertain period defined...');
+  }
+
+};
+
 
 module.exports = uncertainTime;
